a65d28392da2f71258319598745827d5560e15f8
DNA5769 <dennisthomas2002@gmail.com>
2021-10-28 18:18:42 +0530

    Added Image to Landing Component

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -3,7 +3,9 @@ import React from "react"
 const Landing = () => {
 	return (
 		<div>
-			Placeholder
+			<div className="p-4 lg:w-1/2">
+				<img className="w-full" src="/images/hero-right.png" />
+			</div>
 		</div>
 	)
 }

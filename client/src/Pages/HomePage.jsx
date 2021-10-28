44ef4396c9caff34a7946d218a735f062887c16a
DNA5769 <dennisthomas2002@gmail.com>
2021-10-28 17:38:02 +0530

    Removed placeholder from HomePage

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,10 +2,8 @@ import React from "react"
 
 const HomePage = () => {
 	return (
-		<div className="absolute inset-0 grid bg-pink-500 place-content-center">
-			<p className="text-lg font-bold text-white sm:text-4xl">
-				Hello peter
-			</p>
+		<div>
+			
 		</div>
 	)
 }

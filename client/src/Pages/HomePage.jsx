cafd7c1bc852497affbd249e8cdc57d7bf09d116
Neeraj Chatterjee <neeraj.20204123@mnnit.ac.in>
2021-10-29 02:32:29 +0530

    Resolved conflicts

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,24 +1,17 @@
 import React from "react"
-<<<<<<< HEAD
 import Testimonial from "../Components/Testimonial/Testimonial"
-
-const HomePage = () => {
-	return (
-			<div>
-				<Testimonial/>
-			</div>
-=======
 import Landing from "../Components/Landing/Landing"
 import Nav from "../Components/Nav/Nav"
 
 const HomePage = () => {
+
 	return (
 		<div>
 			<Nav />
 			<Landing />
+			<Testimonial/>
 		</div>
->>>>>>> master
 	)
+	
 }
-
 export default HomePage

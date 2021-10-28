374f35147dec9426ab777a994431babe2c33c623
DNA5769 <dennisthomas2002@gmail.com>
2021-10-28 20:33:08 +0530

    Added hover effect for button in Landing Component

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -5,9 +5,9 @@ const Landing = () => {
 		<div className="flex justify-center w-full bg-custom-primary">
 			<div className="max-w-screen-xl text-left text-white lg:flex p-auto">
 				<div className="p-4 font-medium lg:w-1/2">
-					<h1 className="text-4xl md:text-5xl xl:text-7xl">Hotel, car & experiences</h1>
+					<h1 className="text-4xl md:text-5xl xl:text-6xl">Hotel, car & experiences</h1>
 					<p className="mt-8 text-gray-400 md:text-lg">Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation, resort villas, hotels</p>
-					<button className="px-4 py-3 mt-8 text-sm font-medium md:px-6 md:text-base rounded-3xl bg-custom-button-accent">Start your search</button>
+					<button className="px-4 py-3 mt-8 text-sm font-medium md:px-6 md:text-base rounded-3xl bg-custom-button-accent hover:bg-opacity-70">Start your search</button>
 				</div>
 				<div className="p-4 mt-4 lg:w-1/2 lg:pl-10">
 					<img className="w-full" src="/images/hero-right.png" />

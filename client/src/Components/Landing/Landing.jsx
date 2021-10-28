f8e66023f24a42fe0ff97b6afade167b0b96a44d
DNA5769 <dennisthomas2002@gmail.com>
2021-10-28 18:34:06 +0530

    Added content in Landing Component

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -2,8 +2,13 @@ import React from "react"
 
 const Landing = () => {
 	return (
-		<div>
+		<div className="text-left text-white bg-custom-primary">
 			<div className="p-4 lg:w-1/2">
+				<h1 className="text-4xl font-medium">Hotel, car & experiences</h1>
+				<p className="mt-8">Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation, resort villas, hotels</p>
+				<button className="mt-8">Start your search</button>
+			</div>
+			<div className="p-4 mt-4 lg:w-1/2">
 				<img className="w-full" src="/images/hero-right.png" />
 			</div>
 		</div>

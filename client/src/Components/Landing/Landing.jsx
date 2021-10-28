64400b9a7338cf8e66063a44f82aa9c6217c3532
DNA5769 <dennisthomas2002@gmail.com>
2021-10-28 19:12:54 +0530

    Made content in Landing Component responsive

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -2,14 +2,16 @@ import React from "react"
 
 const Landing = () => {
 	return (
-		<div className="text-left text-white bg-custom-primary">
-			<div className="p-4 font-medium lg:w-1/2">
-				<h1 className="text-4xl">Hotel, car & experiences</h1>
-				<p className="mt-8 text-gray-400">Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation, resort villas, hotels</p>
-				<button className="px-4 py-3 mt-8 text-sm font-medium rounded-3xl bg-custom-button-accent">Start your search</button>
-			</div>
-			<div className="p-4 mt-4 lg:w-1/2">
-				<img className="w-full" src="/images/hero-right.png" />
+		<div className="flex justify-center w-full bg-custom-primary">
+			<div className="max-w-screen-xl text-left text-white lg:flex p-auto">
+				<div className="p-4 font-medium lg:w-1/2">
+					<h1 className="text-4xl md:text-5xl xl:text-7xl">Hotel, car & experiences</h1>
+					<p className="mt-8 text-gray-400 md:text-lg">Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation, resort villas, hotels</p>
+					<button className="px-4 py-3 mt-8 text-sm font-medium md:px-6 md:text-base rounded-3xl bg-custom-button-accent">Start your search</button>
+				</div>
+				<div className="p-4 mt-4 lg:w-1/2 lg:pl-10">
+					<img className="w-full" src="/images/hero-right.png" />
+				</div>
 			</div>
 		</div>
 	)

b46553df735c623fe52378ac99587511bea12f8b
ishita1505 <81458413+ishita1505@users.noreply.github.com>
2021-10-29 05:48:34 +0530

    Update HomePage.jsx
    Added Newsletter and footer in Homepage.jsx

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,14 +2,17 @@ import React from "react"
 import Testimonial from "../Components/Testimonial/Testimonial"
 import Landing from "../Components/Landing/Landing"
 import Nav from "../Components/Nav/Nav"
-
+import Newsletter from "../Components/Newsletter/Newsletter"
+import Footer from "../Components/Footer/Footer"
 const HomePage = () => {
 
 	return (
 		<div>
 			<Nav />
 			<Landing />
+			<Newsletter />
 			<Testimonial/>
+			<Footer />
 		</div>
 	)
 	
